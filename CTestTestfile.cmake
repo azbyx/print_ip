382f8f7ae198de057576af5abadd22d4405f65db
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-27 20:23:59 +0000

    Deploy azbyx/print_ip to github.com/azbyx/print_ip.git:gh-pages

--- CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/travis/build/azbyx/print_ip
-# Build directory: /home/travis/build/azbyx/print_ip
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tests_print_ip "tests_print_ip")
-set_tests_properties(tests_print_ip PROPERTIES  _BACKTRACE_TRIPLES "/home/travis/build/azbyx/print_ip/CMakeLists.txt;69;add_test;/home/travis/build/azbyx/print_ip/CMakeLists.txt;0;")
-subdirs("googletest")
