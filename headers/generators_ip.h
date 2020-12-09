a89f3e6ed7cace2402a8f3f7b7ee9f99c633cf6b
azbyx <azbyx@inbox.ru>
2020-12-09 12:27:21 +0300

    Revert "empty"
    This reverts commit a3a019ae0a5a6dbd9f2cdc91a07e045c31802823.

---
+++ headers/generators_ip.h
@@ -0,0 +1,14 @@
+#ifndef GENERATORS_IP_H_INCLUDED
+#define GENERATORS_IP_H_INCLUDED
+
+#include <iostream>
+
+
+
+/*
+template<typename template<class, class> Cont, typename T>
+struct generator_ip {
+
+};
+*/
+#endif // GENERATORS_IP_H_INCLUDED
